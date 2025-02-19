dd09dccc4a4dd37931c0e6bb573aef03b5bf27da
Akshay Kumar <60914748+Akshay8797@users.noreply.github.com>
2025-02-20 02:53:32 +0530

    Strings-8

--- src/strings/LongestDistinctSubstr.java
+++ src/strings/LongestDistinctSubstr.java
@@ -1,5 +1,7 @@
 package strings;
 
+import java.util.Arrays;
+
 import utils.Constants;
 
 /*- Given a string, find the longest substring with distinct characters.
@@ -8,24 +10,54 @@ import utils.Constants;
 public class LongestDistinctSubstr {
 
 	public static void main(String[] args) {
-		String str = "abcdabce";
-		System.out.println(getLongestDistSubstrNaive(str));
+		String str = "abcadbd";
+		System.out.println(getLongestDistSubstrEff(str));
+	}
+
+	// T(n): O(n).
+	private static int getLongestDistSubstrEff(String str) {
+		int n = str.length(), len = 0, i = 0; // i: start indx of substring.
+		int[] prev = new int[Constants.ALL_CHARS];
+		Arrays.fill(prev, -1);
+		for (int j = 0; j < n; j++) { // j: end indx one after another.
+			i = Math.max(i, prev[str.charAt(j)] + 1);
+			int maxEnd = j - i + 1;
+			len = Math.max(len, maxEnd);
+			prev[str.charAt(j)] = j;
+		}
+		return len;
+	}
+
+	// T(n): O(n^2).
+	static int getLongestDistSubstrBetter(String str) {
+		int n = str.length(), len = 0;
+		for (int i = 0; i < n; i++) {
+			boolean[] visited = new boolean[Constants.ALL_CHARS];
+			for (int j = i; j < n; j++) {
+				if (visited[str.charAt(j)])
+					break;
+				len = Math.max(len, j - i + 1);
+				visited[str.charAt(j)] = true;
+			}
+		}
+		return len;
 	}
 
 	// T(n): O(n^3) - Generate all substrings.
-	private static int getLongestDistSubstrNaive(String str) {
+	static int getLongestDistSubstrNaive(String str) {
 		int n = str.length(), len = 0;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				if (areDistinct(str, i, j))
-					len = Math.max(len, j - i + 1);
+				if (!areDistinct(str, i, j)) // not distinct, move to next i.
+					break;
+				len = Math.max(len, j - i + 1);
 			}
 		}
 		return len;
 	}
 
 	// checks if i-j have distinct chars.
-	private static boolean areDistinct(String str, int i, int j) {
+	static boolean areDistinct(String str, int i, int j) {
 		boolean[] visited = new boolean[Constants.ALL_CHARS];
 		for (int k = i; k <= j; k++) {
 			if (visited[str.charAt(k)])
