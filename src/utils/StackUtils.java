ba2812ef0f63f43b1b2ebfe7bcb5e969be339eb9
Akshay Kumar <60914748+Akshay8797@users.noreply.github.com>
2025-03-17 00:35:56 +0530

    Updated errors

--- src/utils/StackUtils.java
+++ src/utils/StackUtils.java
@@ -3,14 +3,30 @@ package utils;
 public class StackUtils {
 
 	public static void stackFullError(int x) {
-		System.out.println("Push " + x + " failed - Stack is full!");
+		System.err.println("Push " + x + " failed - Stack is full!");
+	}
+
+	public static void stackFullError(int x, int stackNum) {
+		System.err.println("Push " + x + " failed - Stack " + stackNum + " is full!");
 	}
 
 	public static Integer stackEmptyError(String action) {
 		if (action.equalsIgnoreCase("Pop"))
-			System.out.println("Pop failed - Stack is empty!");
+			System.err.println("Pop failed - Stack is empty!");
 		else if (action.equalsIgnoreCase("Peek"))
-			System.out.println("Peek failed - Stack is empty!");
+			System.err.println("Peek failed - Stack is empty!");
+		else
+			System.err.println("Stack is empty!");
+		return null;
+	}
+
+	public static Integer stackEmptyError(String action, int stackNum) {
+		if (action.equalsIgnoreCase("Pop"))
+			System.err.println("Pop failed - Stack " + stackNum + " is empty!");
+		else if (action.equalsIgnoreCase("Peek"))
+			System.err.println("Peek failed - Stack " + stackNum + " is empty!");
+		else
+			System.err.println("Stack " + stackNum + " is empty!");
 		return null;
 	}
 
