16cb836ea78aff0f66927a498d575447f2bb4f54
Akshay Kumar <60914748+Akshay8797@users.noreply.github.com>
2025-10-20 02:03:03 +0530

    Graph-12

---
+++ src/graph/ToplogicalSortingDFS.java
@@ -0,0 +1,39 @@
+package graph;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.List;
+
+import utils.GraphUtils;
+
+public class ToplogicalSortingDFS {
+	public static void main(String[] args) {
+		int[][] edges = { { 2, 3 }, { 3, 4 }, { 3 }, {}, {} };
+		int vrtx = 5;
+		List<List<Integer>> adj = GraphUtils.crateAdjList(vrtx, edges);
+		printTopoOrderDFS(adj, vrtx);
+	}
+
+	private static void printTopoOrderDFS(List<List<Integer>> adj, int vrtx) {
+		Deque<Integer> stack = new ArrayDeque<>(vrtx);
+		boolean[] visited = new boolean[vrtx];
+		for (int u = 0; u < vrtx; u++) {
+			if (!visited[u])
+				doDFS(adj, u, stack, visited);
+		}
+		// Print topological order (from stack)
+		while (!stack.isEmpty())
+			System.out.print(stack.pop() + " ");
+
+	}
+
+	private static void doDFS(List<List<Integer>> adj, int u, Deque<Integer> stack, boolean[] visited) {
+		visited[u] = true;
+		for (int v : adj.get(u)) {
+			if (!visited[v])
+				doDFS(adj, v, stack, visited);
+		}
+		stack.push(u); // push after processing descendants
+	}
+
+}
