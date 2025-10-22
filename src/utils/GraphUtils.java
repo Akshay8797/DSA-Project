fe9c905973ba67a2297ca163118707d31b11b8d5
Akshay Kumar <60914748+Akshay8797@users.noreply.github.com>
2025-10-22 23:19:58 +0530

    Updated Graph Utils

--- src/utils/GraphUtils.java
+++ src/utils/GraphUtils.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 public class GraphUtils {
 
-	public static List<List<Integer>> crateAdjList(int v, int[][] neighbors) {
+	/*- Builds the adjacency list representation of a graph.
+	 * 	@param v: the number of vertices in the graph.
+	 * 	@param edges: 2D array where each row contains the neighbors of a vertex.
+	 * 		Row i in edges represents the list of neighbors for vertex i.
+	 * 	@return the adjacency list of the graph where adj.get(i) is the list of neighbors of vertex i.
+	 */
+	public static List<List<Integer>> buildGraph(int v, int[][] edges) {
 		List<List<Integer>> adj = new ArrayList<>(v);
-		if (neighbors.length > v)
+		if (edges.length > v)
 			throw new IllegalArgumentException("Size of adj list cannot exceed no. of vertices");
-		for (int[] row : neighbors) {
+		for (int[] row : edges) {
 			List<Integer> edgeRow = new ArrayList<>();
 			for (int nbr : row)
 				edgeRow.add(nbr);
@@ -28,6 +34,41 @@ public class GraphUtils {
 					System.out.print(" ");
 			}
 			System.out.print("] \n");
+		}
+	}
+
+	public record Edge(int to, int weight) {
+	}
+
+	/*- Builds the adjacency list representation of a directed, weighted graph.
+	 * 	@param v: the number of vertices in the graph.
+	 * 	@param edges: list of int arrays, where each array represents an edge as {from, to, weight}.
+	 * 	@return the adjacency list of the graph.
+	 */
+	public static List<List<Edge>> buildWeightedGraph(int v, List<int[]> edges) {
+		List<List<Edge>> graph = new ArrayList<>();
+		for (int i = 0; i < v; i++)
+			graph.add(new ArrayList<>());
+		for (int[] edge : edges) {
+			if (edge.length != 3)
+				throw new IllegalArgumentException("An edge must have from, to and weight!");
+			int from = edge[0];
+			int to = edge[1];
+			int weight = edge[2];
+			if (from < 0 || from >= v || to < 0 || to >= v)
+				throw new IllegalArgumentException("Vertex index out of bounds!");
+			graph.get(from).add(new Edge(to, weight));
+		}
+		return graph;
+	}
+
+	public static void printWeightedGraph(List<List<Edge>> graph) {
+		for (int i = 0; i < graph.size(); i++) {
+			System.out.print("Node " + i + ": ");
+			for (Edge edge : graph.get(i)) {
+				System.out.print("[to: " + edge.to() + ", weight: " + edge.weight() + "] ");
+			}
+			System.out.println();
 		}
 	}
 
